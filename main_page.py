9216d86b890e26c9f355464116b9ab8448206c8c
tribino <ltribino@gmail.com>
2023-09-05 17:04:22 +0200

    refactor: reorganized code structure

--- main_page.py
+++ main_page.py
@@ -4,52 +4,10 @@ import sys
 import os
 from pandasgui import show
 import warnings
-from scripts.preprocess_data import preprocess_data
-from scripts.preprocess_data import train_model
-
 
 warnings.filterwarnings("ignore")
-
 ruta_proyecto = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(ruta_proyecto, ".."))
-df = pd.read_csv("data/airline_passenger_satisfaction.csv")
-# show(df)
-df = preprocess_data(df)
-model_trained = train_model(df)
-
-
-"""
-    ultima mod
-"""
-
-# Crear un DataFrame para almacenar los datos enviados por el formulario
-data_to_storage = pd.DataFrame(
-    columns=[
-        "id",
-        "Gender",
-        "Customer Type",
-        "Age",
-        "Type of Travel",
-        "Class",
-        "Flight distance",
-        "Inflight wifi service",
-        "Departure/Arrival time convenient",
-        "Ease of Online booking",
-        "Gate location",
-        "Food and drink",
-        "Online boarding",
-        "Seat comfort",
-        "Inflight entertainment",
-        "On-board service",
-        "Leg room service",
-        "Baggage handling",
-        "Check-in service",
-        "Inflight service",
-        "Cleanliness",
-        "Departure Delay in Minutes",
-        "Arrival Delay in Minutes",
-    ]
-)
 
 
 # Función para validar los campos de entrada
@@ -78,15 +36,15 @@ def validate_delay(minutes):
 st.title("Formulario de Satisfacción del Pasajero")
 
 id = st.number_input("ID")
-gender = st.selectbox("Género", ["Female", "Male"])
-customer_type = st.selectbox("Tipo de Cliente", ["Loyal customer", "disloyal customer"])
+gender = st.selectbox("Gender", ["Female", "Male"])
+customer_type = st.selectbox("Customer Type", ["Loyal customer", "disloyal customer"])
 age = st.number_input(
-    "Edad", min_value=7, max_value=80, value=7, step=1, format="%d", key="age"
+    "Age", min_value=7, max_value=80, value=7, step=1, format="%d", key="age"
 )
-type_of_travel = st.selectbox("Tipo de Viaje", ["Personal Travel", "Business Travel"])
-travel_class = st.selectbox("Clase de Viaje", ["Business", "Eco", "Eco Plus"])
+type_of_travel = st.selectbox("Type of Travel", ["Personal Travel", "Business Travel"])
+travel_class = st.selectbox("Class", ["Business", "Eco", "Eco Plus"])
 flight_distance = st.number_input(
-    "Distancia del Vuelo",
+    "Flight distance",
     min_value=31,
     max_value=4982,
     value=31,
@@ -119,11 +77,9 @@ for column in satisfaction_columns:
     )
 
 departure_delay = st.number_input(
-    "Demora en la Salida (minutos)", step=1, key="departure_delay"
+    "Departure Delay in Minutes", step=1, key="departure_delay"
 )
-arrival_delay = st.number_input(
-    "Demora en la Llegada (minutos)", step=1, key="arrival_delay"
-)
+arrival_delay = st.number_input("Arrival Delay in Minutes", step=1, key="arrival_delay")
 
 # Botones para enviar y limpiar el formulario
 if st.button("Enviar"):
@@ -134,7 +90,8 @@ if st.button("Enviar"):
         and validate_delay(arrival_delay)
     ):
         data = {
-            "id": id,
+            "Unnamed: 0": 00,
+            "id": 00,
             "Gender": gender,
             "Customer Type": customer_type,
             "Age": age,
@@ -144,27 +101,9 @@ if st.button("Enviar"):
             **satisfaction_ratings,
             "Departure Delay in Minutes": departure_delay,
             "Arrival Delay in Minutes": arrival_delay,
+            "satisfaction": "nada",
         }
-        # data_to_storage = data_to_storage.append(data, ignore_index=True)
-        new_data = pd.DataFrame(
-            data, index=[0]
-        )  # Crear un DataFrame con un índice explícito
-        data_to_storage = pd.concat([data_to_storage, new_data], ignore_index=True)
-
-
-if st.button("Limpiar"):
-    data_to_storage = pd.DataFrame(columns=data_to_storage.columns)
-
+        test_data = pd.DataFrame([data])
 # Mostrar el DataFrame actualizado
 st.subheader("Último registro agregado:")
-st.write(data_to_storage.tail(1))
-
-st.subheader("Todos los registros agregados:")
-st.write(data_to_storage)
-
-
-"""
-    despues de capturar la data del usuario
-    faltaria agregar la columna unnamed y la columna objetivo o refactorizar el codigo
-    prediction = model_trained.predict(nuevo_registro)
-"""
+# st.write(test_data.tail(1))
